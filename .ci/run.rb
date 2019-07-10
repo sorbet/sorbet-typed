f641eb88d0481b16df5d5b6ef433027cb34c9916
Corey Farwell <coreyf@rwell.org>
2019-07-10 14:53:03 -0400

    Maybe it's available globally?

--- .ci/run.rb
+++ .ci/run.rb
@@ -2,11 +2,9 @@ exclude = ['lib/bundler/all/bundler.rbi']
 
 file_paths = Dir.glob("**/*.rbi").select{|p| !exclude.include?(p)}.map{|p| "\"#{p}\""}.join(' ')
 
-srb_bin = '/usr/local/bundle/bin/srb'
-
 srb_cmd = 'tc'
 
 srb_flags = '--error-black-list 5002'
 
 # `exec` is used so the status code surfaces to CI
-exec("#{srb_bin} #{srb_cmd} #{srb_flags} #{file_paths}")
+exec("srb #{srb_cmd} #{srb_flags} #{file_paths}")
