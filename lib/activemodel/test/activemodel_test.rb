72f80851dfe9720843cc09d617fd1a5fd0713a3e
Connor Shea <connor.james.shea@gmail.com>
2019-08-06 18:34:19 -0600

    Add more test cases to activemodel_test.rb

--- lib/activemodel/test/activemodel_test.rb
+++ lib/activemodel/test/activemodel_test.rb
@@ -2,6 +2,49 @@
 
 module ActiveModelTest
   extend ActiveModel::Validations::HelperMethods
+  extend ActiveModel::Validations::ClassMethods
+
+  validates :bio, length: { maximum: 1000,
+    too_long: "%{count} characters is the maximum allowed" }
+
+  validates :name, length: { minimum: 2 }
+  validates :bio, length: { maximum: 500 }
+  validates :password, length: { in: 6..20 }
+  validates :registration_number, length: { is: 6 }
+
+  validates :points, numericality: true
+  validates :games_played, numericality: { only_integer: true }
+
+  validates :name, :login, :email, presence: true
+  validates :order, presence: true
+  
+  validates :name, :login, :email, absence: true
+
+  validates :email, uniqueness: true
+  validates :name, uniqueness: { scope: :year,
+    message: "should happen once per year" }
+
+  validates :boolean_field_name, inclusion: { in: [true, false] }
+  validates :boolean_field_name, exclusion: { in: [nil] }
+  validates :name, uniqueness: { case_sensitive: false }
+
+  validates :title, length: { is: 5 }, allow_blank: true
+  validates :size, inclusion: { in: %w(small medium large),
+    message: "%{value} is not a valid size" }, allow_nil: true
+
+  validates :age, numericality: { message: "%{value} seems wrong" }
+
+  validates :age, numericality: true, on: :update
+  
+  
+  validates :token, presence: true, uniqueness: true, strict: TokenGenerationException
+  
+  validates :card_number, presence: true, if: :paid_with_card?
+
+  # TODO: These are valid but currently fail typechecking.
+  # validates :password, confirmation: true, unless: Proc.new { |a| a.password.blank? }
+  # validates :name, presence: { strict: true }
+  # validates :password, confirmation: true, unless: -> { true }
 
   # Test a variety of uses for the ActiveModel validation methods.
   validates_absence_of :first_name
