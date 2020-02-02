126d05e99786f5a90556e9057db5adb88f3ed801
Daniel Gilchrist <dan@tanda.co>
2020-02-02 11:28:15 +0000

    Added a couple more tests for String#in_time_zone

--- lib/activesupport/all/activesupport_times_test.rb
+++ lib/activesupport/all/activesupport_times_test.rb
@@ -7,6 +7,8 @@ module ActiveSupportTimesTest
   Time.now.in_time_zone(ActiveSupport::TimeZone["Paris"])
   Time.now.in_time_zone(nil).midnight
 
+  Time.now.to_s.in_time_zone.midnight
+  Time.now.to_s.in_time_zone("Paris").midnight
   Time.now.to_s.in_time_zone(ActiveSupport::TimeZone["Paris"]).midnight
   Time.now.to_s.in_time_zone(nil).midnight
 end
