1e42341defe49c5d527f15dc8b5c5fa7ba85662c
caiofilipemr <caiofilipemr@gmail.com>
2020-05-26 15:14:29 -0300

    Added after_update_commit and after_destroy_commit

---
+++ lib/activerecord/>=5/activerecord_test.rb
@@ -0,0 +1,13 @@
+# typed: true
+
+class ActiveRecordCallbacksTest < ApplicationRecord
+
+  after_create_commit :log_create_commit_action
+  after_create_commit :log_user_saved_to_db, if: :author_wants_emails?, unless: Proc.new { |comment| comment.article.ignore_comments? }
+
+  after_update_commit :log_update_commit_action
+  after_update_commit :log_user_saved_to_db, if: :author_wants_emails?, unless: Proc.new { |comment| comment.article.ignore_comments? }
+
+  after_destroy_commit :log_destroy_commit_action
+  after_destroy_commit :log_user_deleted_from_db, if: :author_wants_emails?, unless: Proc.new { |comment| comment.article.ignore_comments? }
+end
