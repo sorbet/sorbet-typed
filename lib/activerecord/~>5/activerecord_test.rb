38d9a34d99ca5a1d77d9bd994b357d37c168f75a
Caio Filipe de Moraes Rodrigues <caiofilipemr@gmail.com>
2020-05-26 15:42:43 -0300

    Added after_update_commit and after_destroy_commit (#251)

--- lib/activerecord/~>5/activerecord_test.rb
+++
@@ -1,7 +0,0 @@
-# typed: true
-
-class ActiveRecordCallbacksTest < ApplicationRecord
-
-  after_create_commit :log_create_commit_action
-  after_create_commit :log_user_saved_to_db, if: :author_wants_emails?, unless: Proc.new { |comment| comment.article.ignore_comments? }
-end
