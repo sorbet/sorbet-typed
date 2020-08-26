2bdae91342cad63c2b929e35045141d67fa9481e
Alex Ghiculescu <alex@tanda.co>
2020-08-26 11:55:32 -0500

    Add index_with (#280)

--- lib/activesupport/>=6/activesupport_enumerable_test.rb
+++ lib/activesupport/>=6/activesupport_enumerable_test.rb
@@ -3,4 +3,7 @@
 module ActiveSupportEnumerableTest
   T.assert_type!([1, 2, 3].index_by(&:odd?), T::Hash[T::Boolean, Integer])
   T.assert_type!(["hello", "world", "sorbet", "here"].index_by {|s| s.length}, T::Hash[Integer, String])
+
+  T.assert_type!([1,2,3].index_with(&:odd?), T::Hash[Integer, T::Boolean])
+  T.assert_type!(["hello", "world", "sorbet", "here"].index_with {|s| s.length}, T::Hash[String, Integer])
 end
