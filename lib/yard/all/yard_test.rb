ade6d93ea41d02207d5e6e8ed10ffd222e0e53f2
Douglas Eichelberger <dduugg@users.noreply.github.com>
2021-05-23 11:56:25 -0700

    Add YARD handlers (#352)

--- lib/yard/all/yard_test.rb
+++ lib/yard/all/yard_test.rb
@@ -1,5 +1,13 @@
 # typed: strict
 
 YARD::CodeObjects::ClassObject.new(YARD::Registry.root, 'X::Y')
-
 # => #<yardoc class X::Y>
+
+YARD::Handlers::Ruby::AttributeHandler.handlers
+# => [#<YARD::Handlers::Ruby::MethodCallWrapper:0x00007f8b198e52a0 @name="attr">,
+#     #<YARD::Handlers::Ruby::MethodCallWrapper:0x00007f8b198e4fd0 @name="attr_reader">,
+#     #<YARD::Handlers::Ruby::MethodCallWrapper:0x00007f8b198e4dc8 @name="attr_writer">,
+#     #<YARD::Handlers::Ruby::MethodCallWrapper:0x00007f8b198e4c60 @name="attr_accessor">]
+
+YARD::Handlers::Ruby::MethodHandler.handlers
+# => [:def, :defs]
